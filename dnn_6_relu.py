cae6f800d558d6b06cf906d7aba9601faa7a61a9
wada <mituhashitakashi@gmail.com>
2016-11-15 11:28:12 +0900

    add new network model

---
+++ dnn_6_relu.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+
+
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import numpy as np
+
+class Q_DNN(chainer.Chain):
+    
+    modelname = 'dnn6_relu'
+    layer_num = 6
+
+    
+    def __init__(self, input_num, hidden_num,num_of_actions):
+        self.input_num = input_num
+        self.hidden_num = hidden_num
+        self.num_of_actions = num_of_actions
+        
+        super(Q_DNN, self).__init__(
+            fc1=L.Linear(self.input_num, self.hidden_num),
+            fc2=L.Linear(self.hidden_num, self.hidden_num),
+            fc3=L.Linear(self.hidden_num, self.hidden_num),
+            fc4=L.Linear(self.hidden_num, self.hidden_num),
+            fc5=L.Linear(self.hidden_num, self.hidden_num),
+            q_value=L.Linear(self.hidden_num, self.num_of_actions,
+                             initialW=np.zeros((self.num_of_actions, self.hidden_num),
+                                               dtype=np.float32))
+            
+        )
+        
+    def Q_func(self, state):
+        
+        
+        h = F.relu(self.fc1(state))
+        h = F.relu(self.fc2(h))  
+        h = F.relu(self.fc3(h))
+        h = F.relu(self.fc4(h))
+        h = F.relu(self.fc5(h))
+        Q = self.q_value(h)
+
+        return Q
+        
+    
+
+        
